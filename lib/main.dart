72ccad97ddf7366f5516b174a6cbb0bdc280250b
M.L.Akila Liyanage <mlakilaliyanage@gmail.com>
2021-07-29 18:13:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pizzahut/pages/Login.dart';
+import 'package:pizzahut/pages/product_page.dart';
+import 'package:pizzahut/pages/splash_page.dart';
+import 'package:pizzahut/pages/welcome.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/welcome',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => Login(),
+        '/product': (context) => Home(),
+        '/welcome': (context) => Welcome(),
+      },
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.red[100]),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.red[100],
+                        radius: 100.0,  
+                        child: Image.asset('assets/images/logo.png'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 250.0),
+                      ),
+                      Text(
+                        "Pizza Hut",
+                        style: TextStyle(
+                            color: Colors.redAccent,
+                            // fontWeight: FontWeight.bold,
+                            fontSize: 30.0),
+                      )
+                    ],),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
