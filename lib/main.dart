2e1fe311e14cb973408995814fe8711284a40060
udithajanadara <developer1@chanodil.com>
2021-08-09 12:32:58 +0530

    login page refactored

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/welcome',
+      initialRoute: '/login',
       routes: {
         '/': (context) => SplashScreen(),
         '/login': (context) => Login(),
