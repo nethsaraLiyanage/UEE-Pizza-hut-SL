45cf5d3c1901c2573eaa7ced88da77ffd7e906e7
M.L.Akila Liyanage <IT19120812@my.sliit.lk>
2021-09-19 23:38:57 +0530

    cart UI 100%

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/home',
+      initialRoute: '/register',
       routes: {
         '/': (context) => SplashScreen(),
         '/home': (context) => MainScreen(),
