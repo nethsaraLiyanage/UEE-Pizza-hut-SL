9f7dc52f653310f88c7b6b835cac139cd559b259
unknown <dhmmpthammita@gmail.com>
2021-09-20 20:03:17 +0530

    Payment page 80% Compleated

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:pizzahut/pages/splash_page.dart';
 import 'package:pizzahut/pages/welcome.dart';
 import 'package:pizzahut/pages/Main_screen.dart';
 import 'package:pizzahut/pages/Location.dart';
+import 'package:pizzahut/pages/Payment.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/location',
+      initialRoute: '/payment',
       routes: {
         '/': (context) => SplashScreen(),
         '/home': (context) => MainScreen(),
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         '/register': (context) => Register(),
         '/cart': (context) => Cart(),
         '/location':(context) => Location(),
+        '/payment':(context) => Payment(),
       },
     );
   }
