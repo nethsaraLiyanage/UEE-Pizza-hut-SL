930a2313b7a268d00ecbf5276976329711ea89a2
UdithaJ <udithaj.98@gmail.com>
2021-09-28 11:26:32 +0530

    Style changes

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     Timer(Duration(seconds: 6), (){
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=> MainScreen()));
+       Navigator.pushNamed(context, '/login');
     });
   }
   @override
