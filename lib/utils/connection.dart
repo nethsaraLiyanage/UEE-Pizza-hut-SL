e3404516aed6e9f8df557f402742233874d4c734
UdithaJ <udithaj.98@gmail.com>
2021-10-05 13:08:00 +0530

    base url added

---
+++ lib/utils/connection.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class Connection {
+  static final baseUrl = "http://678c-2402-4000-2381-5630-95c4-2aa3-4350-b4ac.ngrok.io";
+}
