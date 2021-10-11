842007e209a0983ded572ea468555575e20ba4dd
Nethsara Liyanage <tharusharb1012@gmail.com>
2021-10-11 07:36:35 +0530

    prod page 2

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {
         '/': (context) => SplashScreen(),
